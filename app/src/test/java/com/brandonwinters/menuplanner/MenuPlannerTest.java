8d94f413f3bf75f782cbb161734e6da2a53f36ff
Brandon Winters <b_winters@yahoo.com>
2024-08-27 23:49:07 -0600

    end of day 1

--- app/src/test/java/com/brandonwinters/menuplanner/MenuPlannerTest.java
+++ app/src/test/java/com/brandonwinters/menuplanner/MenuPlannerTest.java
@@ -9,9 +9,9 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class MenuPlannerTest {
     @Test
-    public void addition_isCorrect () {
-        assertEquals(4, 2 + 2);
+    public void addStoreInformation () {
+        assertTrue(true);
     }
 }
